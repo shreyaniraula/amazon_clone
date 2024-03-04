0959a4f9ceee10c073bc8207e4dcb67df3cd67da
Shreya <shreyaniraula33@gmail.com>
2024-03-04 13:44:31 +0545

    Fixed user signup

--- lib/constants/error_handling.dart
+++ lib/constants/error_handling.dart
@@ -15,7 +15,7 @@ void httpErrorHandle({
       onSuccess();
       break;
     case 400:
-      showSnackBar(context, jsonDecode(response.body)['msg']);
+      showSnackBar(context, jsonDecode(response.body)['message']);
       break;
     case 500:
       showSnackBar(context, jsonDecode(response.body)['error']);
